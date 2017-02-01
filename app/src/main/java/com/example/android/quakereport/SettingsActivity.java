95c5065a72d1c2fb4de01f2081ec047b3cff9018
linucksrox <ericdalyblog@dalydays.com>
2017-02-01 07:25:37 -0500

    Add settings menu to app

---
+++ app/src/main/java/com/example/android/quakereport/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by edaly on 2/1/17.
+ */
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+    }
+
+    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+
+    }
+}
